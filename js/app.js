2e2ea01290758ce96a7d9f82ffdd47e6baf1c932
David van Oudheusden <david@dedaaf.com>
2015-11-22 22:40:29 +0100

    Menu and Google Maps are loaded And also the begining of the model

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+var myGoogleMapsModel = {
+    locationName: 'Scheepvaarsmuseum',
+    streetName: 'Kattenburgerplein 1',
+    zipCode: '1018 KK',
+    City: 'Amsterdam',
+    Description: 'Museum about boats and stuff',
+};
+
