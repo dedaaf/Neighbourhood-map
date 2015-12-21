fc8c325c51e04c8c4fe947287d7176d7c9634e0d
David van Oudheusden <david@dedaaf.com>
2015-12-21 14:39:55 +0100

    beautified files

--- js/app_new.js
+++ js/app_new.js
@@ -1,4 +1,7 @@
-//the locations that will be used
+/*
+ * description Object with different locations from the neighboourhood.
+ * description Some variables are added but not used (for now)
+ */
 var locations = [{
     locationName: 'Scheepvaartmuseum',
     streetName: 'Kattenburgerplein 1',
@@ -46,12 +49,15 @@ var locations = [{
     id: 'Brouwer-t-ij'
 }];
 
+
 //this is the first model
 var mapObj = {};
 
-//initialize the map
+/**
+ * @description Initialize the Google Map
+ * @param {object} canvasMap - The name of the Google Map
+ */
 mapObj.initMap = function() {
-    "use strict";
     var self = this;
     self.canvasMap = new google.maps.Map(document.getElementById('map'), {
         center: {
@@ -60,15 +66,20 @@ mapObj.initMap = function() {
         },
         zoom: 15
     });
-    $('#map').css('height', '400px');
+    $('#map').css('height', '400px'); //overrides Bootstrap settings
     mapObj.createMarkers();
     mapObj.setAllMarkers(self.canvasMap);
     mapObj.createInfoWindow();
 };
 
-//create the markers and its definitions
+
+/**
+ * description Create the markers and its definitions
+ * description Calculate each marker based on its given Lattitude and Longitude
+ * description Also sets each title, description and city name, in this case Amsterdam
+ */
 mapObj.createMarkers = function() {
-    "use strict";
+    'use strict';
 
     var self = this;
     self.marker = null;
@@ -89,9 +100,6 @@ mapObj.createMarkers = function() {
             });
             self.markerArray.push(self.marker); //store all the beautiful markers in an array
 
-         //   self.marker.addListener('click', mapObj.openWindow(self.marker));
-         //   self.marker.addListener('mouseover', mapObj.bounceMarker(self.marker.title));
-
             mapObj.openWindow(self.marker, false);
             mapObj.bounceMarker(self.marker, false);
         } //end for
@@ -99,63 +107,80 @@ mapObj.createMarkers = function() {
     }, self); //end displayAllMarkers
 };
 
-//Put the markers on the map....
+/**
+ * description Takes all the markers from the MarkerArray and puts them onto the map
+ * @param {object} - map - Google Map from initMap()
+ */
 mapObj.setAllMarkers = function(map) {
-    // Set all markers in that we have in the markerArray.
-    // But sometimes this can be empty...hence the opportunity to insert an
+
     for (var i = 0; i < locations.length; i++) {
         this.markerArray()[i].setMap(map);
     }
 }; //end setAllMarkers
 
-//create a GM info window
+/** Create a Google Map info (popup) window */
 mapObj.createInfoWindow = function() {
     this.infowindow = new google.maps.InfoWindow({
         content: '<div id="windowTool"></div>'
     });
 };
 
-//When a list item or marker is clicked (this is checked with the status) the window will be opened
+/**
+ * @description When a list item or marker is clicked (this is checked with @param status) the window will be opened
+ * @param {object} marker - Map Marker object
+ * @param {boolean} status - Status: Sidebar selection or map selection
+ */
+
 mapObj.openWindow = function(marker, status) { //open the GM tooltip
     var self = this;
     //listen to a click and then open the correct window for that marker
-    if(status === false){
-        marker.addListener('click', function(markerO){
+    if (status === false) {
+        marker.addListener('click', function(markerO) {
             openWin();
         });
-    }
-    else{
+    } else {
         openWin();
     }
 
-    function openWin(){
+    /**
+     * @description Close an already opened window
+     * @description Open a new one base on the name of the map and the specific marker
+     * @description Empty everything in the previous window
+     * @description Set and show the title and the description
+     * @description launch funtion displayPhoto from the photo Model
+     */
+    function openWin() {
         self.infowindow.close(); //close other infowindows first
         self.infowindow.open(self.canvasMap, marker); //open the window
         $('#windowTool').empty(); //empty everything first
-        $('<div class="marker_title"><h4>' + marker.title + '</h4>'+
-            '<h5>'+ marker.description +'</h5></div>').appendTo('#windowTool'); //now add data
+        $('<div class="marker_title"><h4>' + marker.title + '</h4>' +
+            '<h5>' + marker.description + '</h5></div>').appendTo('#windowTool'); //now add data
         photoObj.displayPhoto(marker); //get the photos from the internet
 
     }
 };
 
-//bounce the marker. But different models use this so a status check is being done
-//false means the mapModel and true the sidebarModel
+/**
+ * @description Bounce the selected marker. But different models use this so a status check is being done
+ * @param {object} marker - Selected Map Marker object
+ * @param {boolean} status - Status: Sidebar selection or map selection
+ */
 mapObj.bounceMarker = function(marker, status) {
-    console.log(marker);
 
-
-    if(status === false){
-        marker.addListener('mouseover', function(markerON){
-        //bounce the marker when mouseover the marker on the map
+    if (status === false) {
+        marker.addListener('mouseover', function(markerON) {
+            //bounce the marker when mouseover the marker on the map
             bounce(marker);
         });
-    }else{
+    } else {
         //bounce the marker when mouseover over the list items
         bounce(marker);
     }
 
-
+    /**
+     * @description Bounce the selected marker.
+     * @param {object} marker - Selected Map Marker object
+     */
     function bounce(marker) {
         marker.setAnimation(google.maps.Animation.BOUNCE);
         //bounce the actual marker and unbounce it after a couple of sec.
@@ -163,29 +188,47 @@ mapObj.bounceMarker = function(marker, status) {
             unbounce(marker);
         }, 2500);
     }
-
+    /**
+     * @description Let the bounce of the selected marker STOP
+     * @param {object} marker - Selected Map Marker object
+     */
     function unbounce(marker) {
         marker.setAnimation(null);
     }
 };
 
+/**
+ * @description creation of the Sidebar Model
+ */
 var sidebarObj = {
 
 };
 
-//initialize sidebar Model
+/**
+ * @description Initialize the sidebar Model
+ */
 sidebarObj.init = function() {
 
     this.places = ko.observableArray(locations.slice(0)); //array to hold the locations
     this.query = ko.observable(''); //oberve the search field
 
 };
-sidebarObj.init();//launch it
+sidebarObj.init(); //launch it
 
-
-//search function to search through the locations
+/**
+ * @description Search function to search through the locations
+ * @param {object} marker - Selected Map Marker object
+ * @param {boolean} status - Status: Sidebar selection or map selection
+ */
+//
 sidebarObj.searchLoc = function() {
     var self = this;
+
+    /**
+     * @description Search function to search through the locations
+     * @param {object} value - takes the value from the searchbar
+     *
+     */
     self.query.subscribe(search = function(value) {
         self.places.removeAll(); // remove all the current places, which removes them from the view
         mapObj.setAllMarkers(null); //also remove the markers from the map
@@ -196,7 +239,7 @@ sidebarObj.searchLoc = function() {
                 self.places.push(locations[i]);
 
                 self.marker = mapObj.markerArray()[i]; //get the marker data from the array in the mapModel
-                self.marker.setMap(mapObj.canvasMap); //set the array
+                self.marker.setMap(mapObj.canvasMap); //set the markers back onto the map
             } //end if
         } //end for
         // }//end else
@@ -205,52 +248,74 @@ sidebarObj.searchLoc = function() {
 
 sidebarObj.searchLoc(); //activate the searchLoc function
 
-//when a loction is highlighted with the mousee we know which marker it is.
-//Do the mouse over and click (when clicked) and bounce the marker and open a window
+
+/**
+ * @description When a location in the sidebar is highlighted with the mouse we know what marker corresponds with that name
+ * @description Do the mouse over and click (when clicked) and bounce the marker and open a window
+ * @param {object} element - @location object
+ * @param {jQuery object} domEl - returns if mouse is hovered
+ */
 sidebarObj.selectLocation = function(element, domEl) { //perform some styling.
+    console.log('element', element);
+    console.log('domEl', domEl);
     var current = domEl.currentTarget;
     $(current).css('color', '#fff').css('cursor', 'pointer').css('background-color', '#5CB85C');
     //sidebarObj.bounceMarker(element.locationName);
     showMarker(element.locationName);
 
-    function showMarker(locationName){
+    //show the marker that is selected based on the location name
+    function showMarker(locationName) {
         for (var i = 0; i < mapObj.markerArray().length; i++) {
             if (mapObj.markerArray()[i].title.toLowerCase().indexOf(locationName.toLowerCase()) >= 0) {
                 var marker = mapObj.markerArray()[i];
-                mapObj.bounceMarker(marker , true);
+                mapObj.bounceMarker(marker, true);
             }
         }
     }
 };
 
+/**
+ * @description Undo the selection in the list
+ * @param {object} element - @location object
+ * @param {jQuery object} domEl - returns if mouse is hoveredd
+ */
 sidebarObj.selectLocationUndo = function(element, domEl) {
     $(domEl.currentTarget).css('background-color', '#fff').css('color', '#333');
 };
 
-sidebarObj.openWin = function(element, domEl){
+/**
+ * @description Open the Popup Window for the selected location
+ * @param {object} element - @location object
+ * @param {jQuery object} domEl - returns if mouse is hoveredd
+ */
+sidebarObj.openWin = function(element, domEl) {
     var current = domEl.currentTarget;
     for (var i = 0; i < mapObj.markerArray().length; i++) {
-            if (mapObj.markerArray()[i].title.toLowerCase().indexOf(element.locationName.toLowerCase()) >= 0) {
-                var marker = mapObj.markerArray()[i];
+        if (mapObj.markerArray()[i].title.toLowerCase().indexOf(element.locationName.toLowerCase()) >= 0) {
+            var marker = mapObj.markerArray()[i];
 
-                mapObj.openWindow(marker, true);
-            }
+            mapObj.openWindow(marker, true);
         }
+    }
 };
 
-
-//When the users clicks a marker this function is run to display photo's below the map
+/**
+ * @description When the users clicks a marker this function (or model) is run to display photos below the map
+ */
 var photoObj = {
     showFlickr: ko.observable(true),
     showGetty: ko.observable(false),
-    urls_flickr : ko.observableArray(),
-    urls_getty : ko.observableArray(),
+    urls_flickr: ko.observableArray(),
+    urls_getty: ko.observableArray(),
     noImg: ko.observable(false),
     errorImg: ko.observable(false),
 };
 
 
-//if there is a location found please go ahead and print the pictures
+/**
+ * @description If there is a location found please go ahead and print the pictures
+ * @param { object} [marker] [Google Maps Marker]
+ */
 photoObj.displayPhoto = function(marker) {
     var locationName = marker.title;
 
@@ -263,12 +328,14 @@ photoObj.displayPhoto = function(marker) {
     }
 };
 
-
-//retrieve images from Flickr using its API. The search is done base on the title of the marker
+/**
+ * @description Retrieve images from Flickr using its API. The search is done base on the title of the marker
+ * @param { object} [marker] [Google Maps Marker]
+ */
 photoObj.flickr = function(marker) {
     var self = this;
     var locationName = marker.title;
-    self.urls_flickr([]);//empty array
+    self.urls_flickr([]); //empty array
 
     $('#flickrPhotos').empty(); //empty all the previous photo's
     var string = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e291a839000711cc0d54015ed9636d6a&jsoncallback=?';
@@ -301,35 +368,36 @@ photoObj.flickr = function(marker) {
                         var photosChecked = data.photos.photo[i];
                         var photoUrl = 'https://farm' + photosChecked.farm + '.staticflickr.com/' + photosChecked.server + '/' + photosChecked.id + '_' + photosChecked.secret + '.jpg' + '" >';
                         //$('<img/>').attr('src', photoIsGo).appendTo('#flickrPhotos');
-                        self.urls_flickr.push({url:photoUrl}); //push photos in an array again so that we can use them in the html
+                        self.urls_flickr.push({
+                            url: photoUrl
+                        }); //push photos in an array again so that we can use them in the html
                     }
                 }
 
                 var randPic = self.urls_flickr()[Math.floor(Math.random() * self.urls_flickr().length)]; //randomizer
-                 console.log(randPic);
-                // var phto_win = randPic;
-                // var firstPhoto_img = 'https://farm' + phto_win.farm + '.staticflickr.com/' + phto_win.server + '/' + phto_win.id + '_' + phto_win.secret + '.jpg' + '" >';
+                console.log(randPic);
                 $('<img/>').attr('src', randPic.url).attr('id', 'picP').appendTo('#windowTool');
-            } //end fail check
-            else{
+            } else {
                 photoObj.errorImg(true);
                 $('<span class="label label-danger">Error occured retrieving photos</span>').appendTo('#windowTool');
-             //   $('<span class="label label-danger">Error occured retrieving photos</span>').appendTo('#flickrPhotos');
             }
         } //end function
 
 
     ); //end JSON
 
-    $( document ).ajaxError(function() {
-        $('<span class="label label-danger">Error occured retrieving photos</span>').appendTo( '#gettyPhotos' );
+    $(document).ajaxError(function() {
+        $('<span class="label label-danger">Error occured retrieving photos</span>').appendTo('#gettyPhotos');
     });
 
 };
 
-//this function retrieves pictures from Getty Images. The search is done first by the name of
-//the locations defined in the locations object. If the location is not found we will search the
-//Getty database for City images.
+/**
+ * @description This function retrieves pictures from Getty Images. The search is done first by the name of
+ *              the locations defined in the locations object. If the location is not found we will search the
+ *              Getty database for City images.
+ * @param { object} [marker] [Google Maps Marker]
+ */
 photoObj.gettyImg = function(marker) {
     var self = this;
     var locName = marker.title;
@@ -337,13 +405,13 @@ photoObj.gettyImg = function(marker) {
 
     $('#gettyPhotos').empty(); //empty all the previous photo's
     self.urls_getty([]);
-    var string_n = "https://api.gettyimages.com/v3/search/images?phrase=" + locName ;
+    var string_n = 'https://api.gettyimages.com/v3/search/images?phrase=' + locName;
 
     $.ajax({
         type: 'GET',
         url: string_n,
         headers: {
-            "Api-Key": "vss3dkv8ynztu6zud3wsgeed"
+            'Api-Key': 'vss3dkv8ynztu6zud3wsgeed'
                 // more as you need
         },
 
@@ -355,8 +423,10 @@ photoObj.gettyImg = function(marker) {
 
             var imgURL = imgG.display_sizes[0].uri;
             if (imgCaption !== null) {
-                if (imgCaption.toLowerCase().indexOf(locName.toLowerCase()) >= 0 ) {
-                    self.urls_getty.push({url:imgURL}); //push all the valid urls in an obervable array
+                if (imgCaption.toLowerCase().indexOf(locName.toLowerCase()) >= 0) {
+                    self.urls_getty.push({
+                        url: imgURL
+                    }); //push all the valid urls in an obervable array
                 }
             }
         }
@@ -365,12 +435,12 @@ photoObj.gettyImg = function(marker) {
 
         //if there are no pictures found on this API we can return a statement to
         //the user that we will show random city pictures
-        function check_if_pics_found(){
+        function check_if_pics_found() {
 
-            if(self.urls_getty().length ===0 ){
+            if (self.urls_getty().length === 0) {
                 photoObj.noImg(true);
                 runSameWithDescription(); //run the same parameters but with the city instead of locationName
-            }else{
+            } else {
                 photoObj.noImg(false);
             }
         }
@@ -379,14 +449,14 @@ photoObj.gettyImg = function(marker) {
     //this function retrieves pictures from the same API but with city search query.
     //Now the user is warned that he/she does not see pics from the location but random
     //pics from this city
-    function runSameWithDescription(){
+    function runSameWithDescription() {
         //get images that contain
-        var string_d = "https://api.gettyimages.com/v3/search/images?phrase=" + city;
+        var string_d = 'https://api.gettyimages.com/v3/search/images?phrase=' + city;
         $.ajax({
             type: 'GET',
             url: string_d,
             headers: {
-                "Api-Key": "vss3dkv8ynztu6zud3wsgeed"
+                'Api-Key': 'vss3dkv8ynztu6zud3wsgeed'
                     // more as you need
             },
 
@@ -397,23 +467,20 @@ photoObj.gettyImg = function(marker) {
                 if (imgCaption !== null) {
 
                     var imgURL = imgG.display_sizes[0].uri;
-                    self.urls_getty.push({url:imgURL});
+                    self.urls_getty.push({
+                        url: imgURL
+                    });
                 }
-                // else{
-                //      $('<span class="label label-danger">Error occured retrieving photos</span>').appendTo('#windowTool');
-                //     $('<span class="label label-danger">Error occured retrieving photos</span>').appendTo('#gettyPhotos');
-                // }
             }
         });
     }
 
-
-    $( document ).ajaxError(function() {
-        $('<span class="label label-danger">Error occured retrieving photos</span>').appendTo( '#gettyPhotos' );
+    //error handeling
+    $(document).ajaxError(function() {
+        $('<span class="label label-danger">Error occured retrieving photos</span>').appendTo('#gettyPhotos');
     });
 };
 
 
 ko.applyBindings(mapObj, document.getElementById('map'));
 ko.applyBindings(sidebarObj, document.getElementById('sidebar'));
-//ko.applyBindings(photoObj, document.getElementById('photos'));
